9cb9159f2a96a487fd611c1022a67c745000e6fd
j9k3r <j9k3r@yandex.ru>
2024-02-16 00:49:39 +0300

    Fabric.js обновление логики реактивных объектов #1

--- sorry-room/src/components/editor/Editor.vue
+++ sorry-room/src/components/editor/Editor.vue
@@ -1,50 +1,56 @@
 <script setup lang="ts">
 import { fabric } from 'fabric-all-modules';
-import {onMounted, ref} from "vue";
+import {onMounted, reactive, ref} from "vue";
 
-const canvas = ref()
+const fabricWrap = reactive({
+  canvas: null,
+  layers: [],
+  rect: null,
+  img: null
+})
 
 onMounted(() => {
-    canvas.value = new fabric.Canvas('canvas', {
+    fabricWrap.canvas = new fabric.Canvas('canvas', {
       width: 800,
       height: 600,
       // isDrawingMode: true
     });
-    canvas.value.setOverlayColor("rgba(0,0,255,0.4)",undefined,{erasable:false});
+    fabricWrap.canvas.setOverlayColor("rgba(0,0,255,0.4)",undefined,{erasable:false});
 
-    const rect = new fabric.Circle({ top: 10, left: 100, radius: 75, fill: "green", erasable: true })
+    fabricWrap.rect = new fabric.Circle({ top: 10, left: 100, radius: 75, fill: "green", erasable: true })
 
  const imageUrl = 'https://profil.mos.ru/images/banners/bottom/dit-banner.jpg';  // Замените на вашу ссылку на изображение
   fabric.Image.fromURL(imageUrl, (img) => {
     img.scaleToWidth(200);
-    canvas.value.add(img);
+    fabricWrap.img = img; // сохраняем изображение в fabricWrap.img
+    fabricWrap.canvas.add(fabricWrap.img); // добавляем изображение с fabricWrap.img в canvas
   });
 
 
-   canvas.value.add(rect);
+   fabricWrap.canvas.add(fabricWrap.rect);
 })
 
 function changeAction(target: string) {
 // console.log(target)
   if (target === 'erase') {
-    canvas.value.freeDrawingBrush = new fabric.EraserBrush(canvas.value);
-    canvas.value.freeDrawingBrush.width = 10;
-    canvas.value.isDrawingMode = true;
+    fabricWrap.canvas.freeDrawingBrush = new fabric.EraserBrush(fabricWrap.canvas);
+    fabricWrap.canvas.freeDrawingBrush.width = 10;
+    fabricWrap.canvas.isDrawingMode = true;
   }
   if (target === 'select') {
-    canvas.value.isDrawingMode = false;
-    // const selectedObject = canvas.value.getActiveObject(); // получаем выбранный объект
+    fabricWrap.canvas.isDrawingMode = false;
+    // const selectedObject = fabricWrap.canvas.getActiveObject(); // получаем выбранный объект
     //   selectedObject.setControlsVisibility({
     //     mt: true, // включаем контроллер поворота
     //   });
   }
 
   // if (target = 'brush') {
-    // canvas.value.isDrawingMode = true;
-    // canvas.value.freeDrawingBrush = new fabric.PencilBrush(canvas.value);
-    // canvas.value.freeDrawingBrush.color = "#ffffff"; // установка цвета ластика (белый)
-    // canvas.value.freeDrawingBrush.width = 20; // установка ширины линии, например
-    // canvas.value.freeDrawingBrush.globalCompositeOperation = 'destination-out'; // установка эффекта стирания
+    // fabricWrap.canvas.isDrawingMode = true;
+    // fabricWrap.canvas.freeDrawingBrush = new fabric.PencilBrush(fabricWrap.canvas);
+    // fabricWrap.canvas.freeDrawingBrush.color = "#ffffff"; // установка цвета ластика (белый)
+    // fabricWrap.canvas.freeDrawingBrush.width = 20; // установка ширины линии, например
+    // fabricWrap.canvas.freeDrawingBrush.globalCompositeOperation = 'destination-out'; // установка эффекта стирания
   // }
 }
 </script>

04fb29d7b4a6249be806652e30c2f323f4d7b850
j9k3r <j9k3r@yandex.ru>
2024-02-21 00:39:56 +0300

    Fabric.js обновление логики реактивных объектов #4 добавлена pinia

--- sorry-room/src/components/editor/Editor.vue
+++ sorry-room/src/components/editor/Editor.vue
@@ -1,40 +1,38 @@
 <script setup lang="ts">
 import { fabric } from 'fabric-all-modules';
 import {onMounted, reactive, ref} from "vue";
+import {useEditorStore} from "@/stores/editor";
 
-const fabricWrap = reactive({
+const fabricWrap = {
   canvas: null,
-  layers: []
-})
+};
 
+const editorStore = useEditorStore();
+
+// Ваш код onMounted
 onMounted(() => {
   fabricWrap.canvas = new fabric.Canvas('canvas', {
     width: 800,
     height: 600
   });
-  fabricWrap.canvas.setOverlayColor("rgba(0,0,255,0.4)",undefined,{erasable:false});
 
-  const imageUrl = 'https://profil.mos.ru/images/banners/bottom/dit-banner.jpg';
+  // const imageUrl = 'https://profil.mos.ru/images/banners/bottom/dit-banner.jpg';
+  const imageUrl = 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg';
+  // loadImageToSessionStorageAndAddToCanvas(imageUrl); // загрузка изображения и добавление в canvas
   fabric.Image.fromURL(imageUrl, (img) => {
     img.scaleToWidth(200);
-    fabricWrap.layers.push(img); // Добавляем изображение в массив layers
-    updateCanvas(); // Обновляем canvas
+    const ob = {layer: img, desc: 'img1'}
+    editorStore.layers.push(ob);
+    editorStore.updateCanvas(fabricWrap.canvas); // обновляем canvas после добавления изображения
   });
 
   const rect = new fabric.Circle({ top: 10, left: 100, radius: 75, fill: "green", erasable: true });
-  fabricWrap.layers.push(rect); // Добавляем circle в массив layers
-  updateCanvas(); // Обновляем canvas
+  const ob = { layer: rect, desc: 'figure1'}
+  editorStore.layers.push(ob);
+  // editorStore.layers.push(rect);
+  editorStore.updateCanvas(fabricWrap.canvas); // обновляем canvas после добавления прямоугольника
 });
-
-function updateCanvas() {
-  fabricWrap.canvas.clear(); // Очищаем canvas
-  fabricWrap.layers.forEach(obj => {
-    fabricWrap.canvas.add(obj); // Добавляем объекты из массива layers в canvas
-  });
-}
-
-function changeAction(target: string) {
-// console.log(target)
+const changeAction = (target) => {
   if (target === 'erase') {
     fabricWrap.canvas.freeDrawingBrush = new fabric.EraserBrush(fabricWrap.canvas);
     fabricWrap.canvas.freeDrawingBrush.width = 10;
@@ -42,20 +40,98 @@ function changeAction(target: string) {
   }
   if (target === 'select') {
     fabricWrap.canvas.isDrawingMode = false;
-    // const selectedObject = fabricWrap.canvas.getActiveObject(); // получаем выбранный объект
-    //   selectedObject.setControlsVisibility({
-    //     mt: true, // включаем контроллер поворота
-    //   });
   }
+}
 
-  // if (target = 'brush') {
-    // fabricWrap.canvas.isDrawingMode = true;
-    // fabricWrap.canvas.freeDrawingBrush = new fabric.PencilBrush(fabricWrap.canvas);
-    // fabricWrap.canvas.freeDrawingBrush.color = "#ffffff"; // установка цвета ластика (белый)
-    // fabricWrap.canvas.freeDrawingBrush.width = 20; // установка ширины линии, например
-    // fabricWrap.canvas.freeDrawingBrush.globalCompositeOperation = 'destination-out'; // установка эффекта стирания
+function getThumbnail(object) {
+
+  getThumbnailAsDataURL(object)
+  .then((dataURL) => {
+    console.log(dataURL)
+    // Далее вы можете использовать dataURL для отображения или сохранения миниатюры
+  })
+  .catch((error) => {
+    console.error(error);
+});
+
+  // const img = object._element; // Получаем оригинальный элемент изображения из объекта Proxy
+  // if (img) {
+  //   const tempCanvas = document.createElement('canvas');
+  //   const tempContext = tempCanvas.getContext('2d');
+  //   tempCanvas.width = 50;
+  //   tempCanvas.height = 50;
+  //
+  //   tempContext.drawImage(img, 0, 0, tempCanvas.width, tempCanvas.height);
+  //   const dataURL = tempCanvas.toDataURL('image/png');
+  //   return dataURL; // Возвращаем data URL изображения
+  // } else {
+  //   throw new Error('Failed to get image');
   // }
 }
+
+function getThumbnailAsDataURL(object) {
+
+  return new Promise((resolve, reject) => {
+    if (object.type !== 'circle') {
+      console.log(object)
+      // Создаем временный canvas
+      const tempCanvas = document.createElement('canvas');
+      const tempContext = tempCanvas.getContext('2d');
+      tempCanvas.width = 50;  // Ширина миниатюры
+      tempCanvas.height = 50;  // Высота миниатюры
+
+      // Клонируем объект и масштабируем его
+      const cloneObj = object.clone();
+      // Масштабируем объект
+      // cloneObj.scale(50 / cloneObj.width, 50 / cloneObj.height);
+      cloneObj.set({
+        scaleX: 0.5,  // Масштаб по оси X
+        scaleY: 0.5   // Масштаб по оси Y
+      });
+      const svg = cloneObj.toSVG(); // Получаем SVG представление объекта
+
+      // Создаем изображение в формате data URL из SVG
+      const img = new Image();
+      img.onload = function () {
+        tempContext.drawImage(img, 0, 0, 50, 50);  // Рисуем изображение на canvas
+        const dataURL = tempCanvas.toDataURL('image/png');  // Получаем data URL изображения
+        resolve(dataURL);  // Возвращаем data URL
+      };
+      img.onerror = function () {
+        reject(new Error('Failed to create thumbnail'));
+      };
+      img.src = 'data:image/svg+xml; charset=utf8, ' + encodeURIComponent(svg);  // Создаем изображение из SVG
+    }
+  });
+}
+
+
+// function loadImageToSessionStorageAndAddToCanvas(url) {
+//   // Load image
+//   const image = new Image();
+//   image.crossOrigin = 'Anonymous'; // Устанавливаем настройку для кросс-доменных запросов
+//   image.onload = () => {
+//     // Создаем временный canvas для обработки изображения
+//     const tempCanvas = document.createElement('canvas');
+//     const tempContext = tempCanvas.getContext('2d');
+//     tempCanvas.width = 200;
+//     tempCanvas.height = (image.height / image.width) * tempCanvas.width;
+//     tempContext.drawImage(image, 0, 0, tempCanvas.width, tempCanvas.height);
+//
+//     // Получаем data URL изображения
+//     const dataURL = tempCanvas.toDataURL('image/png');
+//
+//     // Сохраняем data URL в sessionStorage
+//     sessionStorage.setItem('loadedImage', dataURL);
+//
+//     // Добавляем изображение на canvas
+//     fabric.Image.fromURL(dataURL, (img) => {
+//       img.scaleToWidth(200);
+//       fabricWrap.canvas.add(img);
+//     });
+//   };
+//   image.src = url;
+// }
 </script>
 
 <template>
@@ -64,10 +140,22 @@ function changeAction(target: string) {
     <div class="controls">
       <button id="select" type="button" @click="changeAction('select')">select</button>
       <button id="erase" type="button" @click="changeAction('erase')">erase</button>
+      <button id="delete" type="button" @click="editorStore.removeSelectedObject(fabricWrap.canvas)">delete selected</button>
     </div>
   </header>
   <div class="body">
     <canvas id="canvas"></canvas>
+    <section>
+      <div v-for="(item, index) in editorStore.layers" :key="index">
+        <div style="color: red">{{ item.desc }}
+          <img :src="getThumbnail(item.layer)">
+<!--          <img :src="getThumbnail(item.layer)" alt="thumbnail" @click="selectObject(item.layer)">-->
+<!--          <p>{{ item.layer.type }}</p>-->
+          <button @click="editorStore.moveLayerUp(index, fabricWrap.canvas)">up</button>
+          <button @click="editorStore.moveLayerDown(index, fabricWrap.canvas)">down</button>
+        </div>
+      </div>
+    </section>
   </div>
   <footer></footer>
 </section>
---
+++ sorry-room/src/stores/editor.ts
@@ -0,0 +1,55 @@
+import {ref, computed, reactive} from 'vue'
+import { defineStore } from 'pinia'
+
+export const useEditorStore = defineStore('editor', () => {
+  const layers = reactive([])
+
+function updateCanvas(canvas) {
+canvas.clear();
+layers.forEach(obj => {
+  // canvas.add(obj);
+  canvas.add(obj.layer);
+});
+}
+
+function removeSelectedObject(canvas) {
+const selectedObject = canvas.getActiveObject();
+if (selectedObject) {
+  const index = layers.indexOf(selectedObject);
+  if (index !== -1) {
+    layers.splice(index, 1);
+    canvas.remove(selectedObject);
+  }
+}
+}
+
+function moveLayerUp(index, canvas) {
+    // const index = this.fabricWrap.layers.indexOf(selectedObject);
+    if (index !== -1 && index < layers.length - 1) {
+      const selectable = layers.splice(index, 1);
+      layers.splice(index + 1, 0, selectable[0]);
+      updateCanvas(canvas);
+    }
+
+
+  // const selectedObject = this.fabricWrap.canvas.getActiveObject();
+  // if (selectedObject) {
+  //   const index = this.fabricWrap.layers.indexOf(selectedObject);
+  //   if (index !== -1 && index < this.fabricWrap.layers.length - 1) {
+  //     this.fabricWrap.layers.splice(index, 1);
+  //     this.fabricWrap.layers.splice(index + 1, 0, selectedObject);
+  //     this.fabricWrap.updateCanvas();
+  //   }
+  // }
+}
+
+function moveLayerDown(index, canvas) {
+    if (index !== -1 && index != 0) {
+      const selectable = layers.splice(index, 1);
+      layers.splice(index - 1, 0, selectable[0]);
+      updateCanvas(canvas);
+    }
+}
+
+return { layers, updateCanvas, removeSelectedObject, moveLayerUp, moveLayerDown }
+})

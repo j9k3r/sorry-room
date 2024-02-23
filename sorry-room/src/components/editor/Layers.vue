40fe9c7c52d46f5cd612454fde68140699570290
j9k3r <j9k3r@yandex.ru>
2024-02-24 00:54:33 +0300

    Fabric.js обновление логики #9

--- sorry-room/src/components/editor/Layers.vue
+++ sorry-room/src/components/editor/Layers.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import {useEditorStore} from "@/stores/editor";
-import {ref} from "vue";
+import {computed, ref} from "vue";
 
 const props = defineProps({
   fabricWrap: Object
@@ -8,11 +8,39 @@ const props = defineProps({
 
 const editorStore = useEditorStore();
 
+const layersReverse = computed(() => {
+  return editorStore.layers.slice().reverse()
+})
+
 const tab = ref()
-function selectLayer(index) {
+function selectLayer(id) {
+  console.log('123')
+    const index = editorStore.findIndexLayerById(id)
     props.fabricWrap.canvas.discardActiveObject();
     props.fabricWrap.canvas.setActiveObject(props.fabricWrap.canvas.item(index))
     props.fabricWrap.canvas.requestRenderAll();
+}
+
+function wrapInSvg(htmlString) {
+  // Оборачиваем htmlString в элемент SVG
+  const svgString = `<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50">${htmlString}</svg>`;
+
+  const x = convertToBase64(svgString)
+
+  return x
+}
+
+function convertToBase64(svgString) {
+  const svgBlob = new Blob([svgString], {type: 'image/svg+xml'});
+  const reader = new FileReader();
+  reader.readAsDataURL(svgBlob);
+  reader.onloadend = function() {
+    const base64data = reader.result;
+    return base64data
+    // Используйте полученную строку base64data для передачи в атрибут src элемента <img>
+    // Например:
+    // document.getElementById('myImg').src = base64data;
+  }
 }
 </script>
 
@@ -31,18 +59,41 @@ function selectLayer(index) {
     <v-card-text>
       <v-window v-model="tab">
         <v-window-item value="one">
-          one
-          <section>
-            <div v-for="(item, index) in editorStore.layers" :key="index">
-              <div style="color: red">{{ item.desc }}
-                <svg xmlns="http://www.w3.org/2000/svg"width="50" height="50"  viewBox="0 0 50 50" @click="selectLayer(index)"
-                     v-html="item.cloned">
-                </svg>
-                <button @click="editorStore.moveLayerUp(index, props.fabricWrap.canvas)">up</button>
-                <button @click="editorStore.moveLayerDown(index, props.fabricWrap.canvas)">down</button>
-              </div>
-            </div>
-          </section>
+            <v-card
+              class="mx-auto"
+              max-width="425"
+              v-for="(item, index) in layersReverse" :key="index"
+            >
+              <v-list-item>
+               <template v-slot:title class="bg-cyan-darken-1">
+                  index: {{ index }}, id: {{ item.id }}
+                 <v-defaults-provider
+                    :defaults="{
+                      VBtn: {
+                        variant: 'text',
+                        density: 'comfortable',
+                      }
+                    }"
+                  >
+                    <v-btn icon="$vuetify"></v-btn>
+                    <v-btn icon="mdi-arrow-up-bold-outline"  @click="editorStore.moveLayerUp(item.id, props.fabricWrap.canvas)"></v-btn>
+                    <v-btn icon="mdi-arrow-down-bold-outline" @click="editorStore.moveLayerDown(item.id, props.fabricWrap.canvas)"></v-btn>
+                  </v-defaults-provider>
+               </template>
+               <template v-slot:prepend>
+                 <v-icon
+                     @click="selectLayer(item.id)"
+                 >
+                   <svg xmlns="http://www.w3.org/2000/svg"width="50" height="50"  viewBox="0 0 50 50"
+                       v-html="item.cloned">
+                  </svg>
+                 </v-icon>
+               </template>
+                <template v-slot:subtitle>
+                  {{ item.desc }}
+                </template>
+              </v-list-item>
+            </v-card>
         </v-window-item>
 
         <v-window-item value="two">
@@ -55,20 +106,10 @@ function selectLayer(index) {
       </v-window>
     </v-card-text>
   </v-card>
-
-<!--<section>-->
-<!--  <div v-for="(item, index) in editorStore.layers" :key="index">-->
-<!--    <div style="color: red">{{ item.desc }}-->
-<!--      <svg xmlns="http://www.w3.org/2000/svg"width="50" height="50"  viewBox="0 0 50 50" @click="selectLayer(index)"-->
-<!--           v-html="item.cloned">-->
-<!--      </svg>-->
-<!--      <button @click="editorStore.moveLayerUp(index, props.fabricWrap.canvas)">up</button>-->
-<!--      <button @click="editorStore.moveLayerDown(index, props.fabricWrap.canvas)">down</button>-->
-<!--    </div>-->
-<!--  </div>-->
-<!--</section>-->
 </template>
 
 <style scoped>
-
+.v-icon--size-default {
+  font-size: calc(var(--v-icon-size-multiplier) * 3.5em);
+}
 </style>
--- sorry-room/src/stores/editor.ts
+++ sorry-room/src/stores/editor.ts
@@ -4,18 +4,27 @@ import { defineStore } from 'pinia'
 export const useEditorStore = defineStore('editor', () => {
 const layers = reactive([])
 
+function findIndexLayerById(id: number) {
+    const index = layers.findIndex(obj => obj.id === id);
+    return index;
+}
+
 function updateCanvas(canvas) {
     canvas.clear();
     layers.forEach((obj, index) => {
-         obj.layer.clone((cl) => {
-             // cl.centerObject(cl)
-             cl.left = 0
-             cl.top = 0
-             cl.scaleToHeight(50, { aspectRatio: cl.width / cl.height }) // задаем aspectRatio и используем scaleToHeight
-          const svg = cl.toSVG();
-             layers[index].cloned = svg
-         })
+     obj.layer.clone((cl) => {
+      cl.scaleToHeight(50, { aspectRatio: cl.width / cl.height });
+      const box = cl.getBoundingRect(true);
+      const xOffset = (50 - box.width) / 2 - box.left;
+      const yOffset = (50 - box.height) / 2 - box.top;
+      cl.set({ left: cl.left + xOffset, top: cl.top + yOffset });
+      const svg = cl.toSVG();
+        layers[index].cloned = svg;
+      })
       canvas.add(obj.layer);
+     if (obj.id === undefined) {
+         layers[index].id = index
+     }
     });
 }
 
@@ -31,8 +40,10 @@ function removeSelectedObject(canvas) {
     }
 }
 
-function moveLayerUp(index, canvas) {
+function moveLayerUp(id, canvas) {
     // const index = this.fabricWrap.layers.indexOf(selectedObject);
+
+    const index = findIndexLayerById(id)
     if (index !== -1 && index < layers.length - 1) {
       const selectable = layers.splice(index, 1);
       layers.splice(index + 1, 0, selectable[0]);
@@ -40,7 +51,8 @@ function moveLayerUp(index, canvas) {
     }
 }
 
-function moveLayerDown(index, canvas) {
+function moveLayerDown(id, canvas) {
+    const index = findIndexLayerById(id)
     if (index !== -1 && index != 0) {
       const selectable = layers.splice(index, 1);
       layers.splice(index - 1, 0, selectable[0]);
@@ -48,5 +60,5 @@ function moveLayerDown(index, canvas) {
     }
 }
 
-return { layers, updateCanvas, removeSelectedObject, moveLayerUp, moveLayerDown }
+return { layers, findIndexLayerById, updateCanvas, removeSelectedObject, moveLayerUp, moveLayerDown }
 })

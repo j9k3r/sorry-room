e21ee02a4baf2cc442863bd42d2a70a232951f57
j9k3r <j9k3r@yandex.ru>
2024-02-23 11:55:48 +0300

    Fabric.js обновление логики #8 add vuetify

--- sorry-room/src/components/editor/Layers.vue
+++ sorry-room/src/components/editor/Layers.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import {useEditorStore} from "@/stores/editor";
+import {ref} from "vue";
 
 const props = defineProps({
   fabricWrap: Object
 })
 
 const editorStore = useEditorStore();
+
+const tab = ref()
 function selectLayer(index) {
     props.fabricWrap.canvas.discardActiveObject();
     props.fabricWrap.canvas.setActiveObject(props.fabricWrap.canvas.item(index))
@@ -14,17 +17,56 @@ function selectLayer(index) {
 </script>
 
 <template>
-<section>
-      <div v-for="(item, index) in editorStore.layers" :key="index">
-        <div style="color: red">{{ item.desc }}
-          <svg xmlns="http://www.w3.org/2000/svg"width="50" height="50"  viewBox="0 0 50 50" @click="selectLayer(index)"
-               v-html="item.cloned">
-          </svg>
-          <button @click="editorStore.moveLayerUp(index, props.fabricWrap.canvas)">up</button>
-          <button @click="editorStore.moveLayerDown(index, props.fabricWrap.canvas)">down</button>
-        </div>
-      </div>
-</section>
+
+<v-card>
+    <v-tabs
+      v-model="tab"
+      bg-color="primary"
+    >
+      <v-tab value="one">Item One</v-tab>
+      <v-tab value="two">Item Two</v-tab>
+      <v-tab value="three">Item Three</v-tab>
+    </v-tabs>
+
+    <v-card-text>
+      <v-window v-model="tab">
+        <v-window-item value="one">
+          one
+          <section>
+            <div v-for="(item, index) in editorStore.layers" :key="index">
+              <div style="color: red">{{ item.desc }}
+                <svg xmlns="http://www.w3.org/2000/svg"width="50" height="50"  viewBox="0 0 50 50" @click="selectLayer(index)"
+                     v-html="item.cloned">
+                </svg>
+                <button @click="editorStore.moveLayerUp(index, props.fabricWrap.canvas)">up</button>
+                <button @click="editorStore.moveLayerDown(index, props.fabricWrap.canvas)">down</button>
+              </div>
+            </div>
+          </section>
+        </v-window-item>
+
+        <v-window-item value="two">
+          Two
+        </v-window-item>
+
+        <v-window-item value="three">
+          Three
+        </v-window-item>
+      </v-window>
+    </v-card-text>
+  </v-card>
+
+<!--<section>-->
+<!--  <div v-for="(item, index) in editorStore.layers" :key="index">-->
+<!--    <div style="color: red">{{ item.desc }}-->
+<!--      <svg xmlns="http://www.w3.org/2000/svg"width="50" height="50"  viewBox="0 0 50 50" @click="selectLayer(index)"-->
+<!--           v-html="item.cloned">-->
+<!--      </svg>-->
+<!--      <button @click="editorStore.moveLayerUp(index, props.fabricWrap.canvas)">up</button>-->
+<!--      <button @click="editorStore.moveLayerDown(index, props.fabricWrap.canvas)">down</button>-->
+<!--    </div>-->
+<!--  </div>-->
+<!--</section>-->
 </template>
 
 <style scoped>
